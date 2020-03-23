99e84b3fdf0a1910841b6407dd769ab5ea6d1178
Ben O'Neill <ben@benoneill.xyz>
2020-03-23 15:29:53 -0400

    Use config.def.h

--- config.def.h
+++ config.def.h
@@ -5,17 +5,18 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"monospace:size=14",
-	"JoyPixels:pixelsize=10:antialias=true:autohint=true"
+	"JoyPixels:pixelsize=14:antialias=true:autohint=true"
 };
 static const unsigned int bgalpha = 0xe0;
 static const unsigned int fgalpha = OPAQUE;
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { "#39ff14", "#000000" },
+	[SchemeSel] = { "#304937", "#ebdbb2" },
+	[SchemeOut] = { "#000000", "#00FFFF" },
 };
+
 static const unsigned int alphas[SchemeLast][2] = {
 	/*		fgalpha		bgalphga	*/
 	[SchemeNorm] = { fgalpha, bgalpha },
